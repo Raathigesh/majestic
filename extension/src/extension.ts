f9cca821a9d9eaf118e1629bd9427db611373c6f
Raathigeshan Kugarajan <raathigesh92@gmail.com>
2018-05-21 22:46:58 +1000

    Initial VSCode extension

--- extension/src/extension.ts
+++ extension/src/extension.ts
@@ -1,50 +1,119 @@
 "use strict";
 import * as vscode from "vscode";
-const io = require("socket.io-client");
-const socket = io("http://localhost:5687");
-
-socket.on("connect", _ => {
-  socket.emit("hello", vscode.workspace.rootPath);
-});
+import { join } from "path";
+const Sockette = require("sockette");
+const WebSocket = require("ws");
+(global as any).WebSocket = WebSocket;
 
 export function activate(context: vscode.ExtensionContext) {
-  socket.on("startDebug", config => {
-    if (
-      config.rootPath.toLowerCase() !== vscode.workspace.rootPath.toLowerCase()
-    ) {
-      return;
-    }
-
-    const args = [
-      "--runInBand",
-      config.fileName,
-      "--testNamePattern",
-      config.identifier
-    ];
-    if (config.pathToConfig) {
-      args.push("--config", config.pathToConfig);
-    }
-
-    const port = Math.floor(Math.random() * 20000) + 10000;
-    const configuration = {
-      name: "TestRunner",
-      type: "node",
-      request: "launch",
-      program: config.program,
-      args,
-      runtimeArgs: ["--inspect-brk=" + port],
-      port,
-      protocol: "inspector",
-      console: "integratedTerminal",
-      smartStep: true,
-      sourceMaps: true
-    };
-
-    vscode.debug.startDebugging(
-      vscode.workspace.workspaceFolders[0],
-      configuration
-    );
+  let connectionPromise = new Promise(resolve => {
+    const ws = new Sockette("ws://localhost:7777", {
+      timeout: 5e3,
+      onopen: e => {
+        ws.send(
+          JSON.stringify({
+            source: "vscode-majestic",
+            event: "workspace-path",
+            payload: vscode.workspace.rootPath
+          })
+        );
+        resolve(ws);
+      },
+      onreconnect: () => {
+        ws.send(
+          JSON.stringify({
+            source: "vscode-majestic",
+            event: "workspace-path",
+            payload: vscode.workspace.rootPath
+          })
+        );
+      },
+      onclose: e => {
+        debugger;
+      },
+      onerror: e => {
+        debugger;
+      }
+    });
   });
+  connectionPromise.then((connection: any) => {
+    connection.send(
+      JSON.stringify({
+        source: "vscode-majestic",
+        event: "workspace-path",
+        payload: vscode.workspace.rootPath
+      })
+    );
+
+    connection.on("message", messageString => {
+      const message = JSON.parse(messageString);
+      if (
+        message.source === "majestic" &&
+        message.event === "start-debugging"
+      ) {
+        startDebugging(message.payload);
+      } else if (
+        message.source === "majestic" &&
+        message.event === "get-root-path"
+      ) {
+        connection.send(
+          JSON.stringify({
+            source: "vscode-majestic",
+            event: "workspace-path",
+            payload: vscode.workspace.rootPath
+          })
+        );
+      }
+    });
+  });
+}
+
+function startDebugging(config) {
+  if (
+    config.rootPath.toLowerCase() !== vscode.workspace.rootPath.toLowerCase()
+  ) {
+    return;
+  }
+
+  const args = [
+    "--runInBand",
+    getTestPatternForPath(config.fileName),
+    "--testNamePattern",
+    config.identifier
+  ];
+  if (config.pathToConfig) {
+    args.push("--config", config.pathToConfig);
+  }
+
+  const port = Math.floor(Math.random() * 20000) + 10000;
+  const configuration = {
+    name: "TestRunner",
+    type: "node",
+    request: "launch",
+    program: join(config.rootPath, config.program),
+    args,
+    runtimeArgs: ["--inspect-brk=" + port],
+    port,
+    protocol: "inspector",
+    console: "integratedTerminal",
+    smartStep: true,
+    sourceMaps: true
+  };
+
+  vscode.debug.startDebugging(
+    vscode.workspace.workspaceFolders[0],
+    configuration
+  );
+}
+
+function getTestPatternForPath(filePath: string) {
+  let replacePattern = /\//g;
+
+  if (process.platform === "win32") {
+    replacePattern = /\\/g;
+  }
+
+  return `^${filePath.replace(replacePattern, ".")}$`;
 }
 
 // this method is called when your extension is deactivated
