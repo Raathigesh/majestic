f7d1182db6f27dc7e977d89cc18fb821bda23ea7
Greg Veres <greg.veres@rogers.com>
2020-05-18 17:19:59 -0400

    dimmed the results information when test running
    I wanted to provide an additional visual cue to the user that the results
    they were looking at were out of date, but still alow them to view the
    results while the other results were being generated.
    
    Using the information from this SO question:
    https://stackoverflow.com/questions/48502647/conditional-rendering-in-styled-components
    I added a dim property to the Content panel that is triggered
    on isUpdating.

--- ui/test-file/index.tsx
+++ ui/test-file/index.tsx
@@ -28,6 +28,10 @@ const Container = styled.div<any>`
 const Content = styled.div`
   overflow: auto;
   height: calc(100vh - 118px);
+
+  ${({ dim }: any) => dim && `
+  opacity: .5;
+  `}
 `;
 
 const TestItemsContainer = styled.div`
@@ -128,7 +132,7 @@ function TestFile({ selectedFilePath, isRunning, projectRoot, onStop }: Props) {
           updateSnapshot();
         }}
       />
-      <Content>
+      <Content dim={isUpdating}>
         {result && result.testResults && result.testResults.length === 0 && (
           <ErrorPanel failureMessage={result && result.failureMessage} />
         )}
